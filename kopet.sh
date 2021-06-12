e7fe8fcb56d9ec44efd1df1b1162780944d041b0
rizoa <hanirizo@gmail.com>
2021-06-13 00:33:54 +0700

    crot 2

--- kopet.sh
+++ kopet.sh
@@ -2,4 +2,8 @@
 
 echo 'jancuk'
 
-docker login -u afnia -p kopet1234
+docker login quay.io -u hanirizo -p kopet1234
+
+docker build -t quay.io/hanirizo/kontlo .
+docker push quay.io/hanirizo/kontlo
+
